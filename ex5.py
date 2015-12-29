da9b73c6187a8eb13d0ab501016b1939556ba5ae
S.Krishnan <skrish22195@gmail.com>
2015-12-29 17:50:55 +0530

    First 5 exercises

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name = "S.KRISHNAN"
+my_age = 20
+my_height = 179 #cms
+my_weight = 60 #kgs
+my_eyes = "Black"
+my_teeth = "White"
+my_hair = "Black"
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d %d ,and %d I get %d"%(my_age,my_height,my_weight,my_age+my_height+my_weight)
